35d9a74c320f472a202c7433ff2f6a8cf4548857
Alexander Woods <alex.w@ware2go.co>
2020-04-22 16:14:52 -0400

    Add Rookout

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import * as rookout from 'rookout';
 import { AppModule } from './app.module';
+
+rookout.start()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
